246579ee11a72430971431845c6f43e8d83a23d1
sunnybhandal <sunnybhandal123@gmail.com>
2023-05-10 20:10:06 -0700

    yay

--- app/components/Movies.jsx
+++ app/components/Movies.jsx
@@ -4,27 +4,22 @@ export default function Movies({ movies }) {
   // console.log(movies);
 
   const limitOverview = (overview) => {
-    // Remove leading and trailing whitespaces
-    var str = overview.trim().split(" ").slice(0, 25);
-
-    // Join the words back into a string
-    var result = str.join(" ");
-
-    return result;
+    return overview.trim().split(" ").slice(0, 20).join(" ");
   };
 
   return (
     <div className="grid grid-rows-4 grid-flow-col gap-4 pt-4">
       {movies.map((movie) => (
-        <div key={movie.id} className="relative border-2 border-white h-full">
-          <span className="absolute left-0 top-0">Add</span>
-          <img className=" w-full" src={API_IMG + movie.poster_path} />
+        <div className="flex flex-col items-center text-center">
+          <div
+            key={movie.id}
+            className="relative cursor-pointer border-2 border-white h-80 w-60 rounded-md"
+          >
+            <span className="absolute left-0 top-0">Add</span>
+            <img className="h-full w-full" src={API_IMG + movie.poster_path} />
+          </div>
           <div className="p-2">
             <h2>{movie.title}</h2>
-            <p className="text-sm">{limitOverview(movie.overview)}...</p>
-            <span className="absolute cursor-pointer bottom-0 pb-2">
-              View More -
-            </span>
           </div>
         </div>
       ))}
