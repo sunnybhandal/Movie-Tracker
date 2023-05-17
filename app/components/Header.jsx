548042c35aeec961db2b0b3a6137d8ff0c3e240c
sunnybhandal <sunnybhandal123@gmail.com>
2023-05-17 13:12:37 -0500

    did other work

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function Header({ setPage }) {
   const router = useRouter();
+  // console.log('fun')
 
   const handleHome = () => {
     router.push("/");
