f9905ecba50729494424f5a77f5524fd8cc14533
sunnybhandal <sunnybhandal123@gmail.com>
2023-05-22 20:21:28 -0600

    movie detail page setup

--- app/components/Movies.jsx
+++ app/components/Movies.jsx
@@ -1,8 +1,15 @@
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Movies({ movies }) {
+  const router = useRouter();
   const [movieList, setMovieList] = useState([]);
+
+  const movieDetails = (id) => {
+    router.push(`/${id}`);
+    // setPage("");
+  };
 
   const addMovie = (id) => {
     if (movieList.includes(id)) return;
@@ -15,7 +22,11 @@ export default function Movies({ movies }) {
       {movies.map((movie) => (
         <div key={movie.id} className="flex flex-col pb-4">
           <div className="relative cursor-pointer border-2 border-white w-60 rounded-md">
-            <img className="h-72 w-72" src={API_IMG + movie.poster_path} />
+            <img
+              className="h-72 w-72"
+              src={API_IMG + movie.poster_path}
+              onClick={() => movieDetails(movie.id)}
+            />
             <div
               onClick={() => addMovie(movie.id)}
               className="justify-center h-8 flex items-center bg-green-500 hover:bg-green-700"
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import Movies from "./components/Movies";
 import LoadingPage from "./loading";
 import { useState, useEffect } from "react";
 const API_KEY = process.env.API_KEY;
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -11,7 +10,9 @@ export default function Home() {
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const res = await fetch(API_URL);
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      );
       const data = await res.json();
       setMovies(data.results);
       setLoading(false);
