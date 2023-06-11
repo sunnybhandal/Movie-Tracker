4b110ba1308c53f86d774fafeadd34050b104dba
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-10 20:47:33 -0600

    saves list to db

--- pages/api/saveList.jsx
+++ pages/api/saveList.jsx
@@ -4,30 +4,25 @@ import { authOptions } from "./auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 
 export default async function handler(req, res) {
-  console.log(req.body, res.method, "save list");
+  // console.log(req.body, res.method, "save list");
   if (req.method === "POST") {
+    dbConnect();
+
     const session = await getServerSession(req, res, authOptions);
+    // console.log("SESSION", session.user);
 
-    console.log("SESSION", session);
-    // const db = dbConnect();
-    // console.log(db);
-    // const collection = db.collection("users");
-    // console.log(collection);
-    // await collection.insertOne(movieList);
-    // res.status(201).json({ doc });
+    const { movieList } = req.body;
 
-    try {
-      const { movieList } = req.body;
-      const db = await connectToDatabase();
-      console.log(db);
-      const collection = db.collection("users");
-      console.log(collection);
+    const email = session.user.email;
+    // console.log(user);
+    await User.updateOne({ email }, { movieList: movieList });
+    res.status(201).json({ message: "Object saved successfully" });
 
-      await collection.insertOne(movieList);
-      res.status(200).json({ message: "Object saved successfully" });
-    } catch (error) {
-      res.status(500).json({ message: "Error saving object" });
-    }
+    // try {
+    //   res.status(200).json({ message: "Object saved successfully" });
+    // } catch (error) {
+    //   res.status(500).json({ message: "Error saving object" });
+    // }
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
   }
