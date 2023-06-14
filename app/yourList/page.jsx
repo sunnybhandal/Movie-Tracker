915ab88a1ae5f69f3493185423c3d93b061b90ad
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-13 20:49:38 -0600

    basic movie retrieval from a single id

--- app/yourList/page.jsx
+++ app/yourList/page.jsx
@@ -1,44 +1,69 @@
 "use client";
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
+const API_KEY = process.env.API_KEY;
 import { useEffect, useState } from "react";
 import Modal from "../components/Modal";
 import Card from "../components/Card";
 import { useRouter } from "next/navigation";
+import LoadingPage from "../loading";
 
 export default function MovieList() {
   const router = useRouter();
-  const [userMovies, setUserMovies] = useState(null);
+  const [userMovies, setUserMovies] = useState([]);
+  const [movies, setMovies] = useState();
   const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState();
   const [loading, setLoading] = useState(true);
+  const testData = [713704, 879444, 667538, 758323];
+  const testId = 758323;
+  console.log(userMovies, "user movies");
 
-  // useEffect(() => {
-  //   const fetchMovies = async () => {
-  //     const res = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}`);
-  //     const data = await res.json();
-  //     setUserMovies(data.results);
-  //     setLoading(false);
-  //   };
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${testId}?api_key=${API_KEY}`
+      );
+      const data = await res.json();
+      console.log(data, "data for movie id");
+      setUserMovies(data);
+      setLoading(false);
+    };
 
-  //   fetchMovies();
-  // }, []);
+    getMovieDetails();
+  }, []);
+
+  const handleOpenModal = (movie) => {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+    setShowModal(false);
+  };
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
       {userMovies ? (
         <div className="sm:grid grid-flow-col auto-cols-auto sm:grid-rows-5 xl:grid-rows-4 2xl:grid-rows-5 pt-4 justify-between ">
-          {movies.map((movie) => (
+          {/* {userMovies.map((movie) => (
             <div key={movie.id} className="pb-4">
               <Card
                 api_img={API_IMG}
                 movie={movie}
                 handleOpenModal={handleOpenModal}
-                addMovie={addMovie}
-                removeMovie={removeMovie}
-                movieList={movieList}
-                loggedIn={data}
-                goLoginPage={goLoginPage}
               />
             </div>
-          ))}
+          ))} */}
+          <Card
+            api_img={API_IMG}
+            movie={userMovies}
+            handleOpenModal={handleOpenModal}
+          />
         </div>
       ) : (
         <div className="w-full h-80 flex justify-center items-center">
